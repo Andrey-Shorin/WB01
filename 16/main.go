62680123924d9e9f2f4655b90bf2e7e9a5d818cf
Ytterby <ytterby@MacBook-Pro-Ytterby.local>
2024-05-06 18:39:57 +0300

    fix

--- 16/main.go
+++ 16/main.go
@@ -6,13 +6,42 @@ import (
 	"sort"
 )
 
+func quickSort(arr []int, start, fin int) []int {
+	if start < fin {
+		var p int
+		arr, p = partition(arr, start, fin)
+		arr = quickSort(arr, start, p-1)
+		arr = quickSort(arr, p+1, fin)
+	}
+	return arr
+}
+
+func partition(arr []int, start, fin int) ([]int, int) {
+	pivot := arr[fin]
+	i := start
+	for j := start; j < fin; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[fin] = arr[fin], arr[i]
+	return arr, i
+}
+
+func QuickSortStart(arr []int) []int {
+	return quickSort(arr, 0, len(arr)-1)
+}
+
 func main() {
 	var slice = make([]int, 30)
 	for i := 0; i < 30; i++ {
 		slice[i] = rand.Intn(999)
 	}
+	slice2 := append([]int(nil), slice...)
 	fmt.Println(slice)
 	sort.Slice(slice, func(a, b int) bool { return slice[a] < slice[b] })
 	fmt.Println(slice)
+	fmt.Println(QuickSortStart(slice2))
 
 }
--- 17/main.go
+++ 17/main.go
@@ -6,6 +6,23 @@ import (
 	"sort"
 )
 
+func BinarySearch(arr []int, val int) int {
+	left, right := 0, len(arr)
+	mid := (left + right) / 2
+
+	for left < right {
+		if arr[mid] == val {
+			return mid
+		} else if arr[mid] < val {
+			left = mid
+		} else {
+			right = mid
+		}
+		mid = (left + right) / 2
+	}
+	return -1
+}
+
 func main() {
 	var slice = make(sort.IntSlice, 30, 30)
 	for i := 0; i < 30; i++ {
@@ -15,6 +32,8 @@ func main() {
 	sort.Slice(slice, func(a, b int) bool { return slice[a] < slice[b] })
 	fmt.Println(slice)
 	find := slice.Search(x)
+	y := BinarySearch(slice, x)
 	fmt.Println("find ", slice[find], " on ", find, "search for ", x)
+	fmt.Println("find ", slice[y], " on ", y, "search for ", x)
 
 }
